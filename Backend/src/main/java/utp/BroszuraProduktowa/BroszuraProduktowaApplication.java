3c49a56e952341dd570f896f8f015d749f7a6e33
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2020-11-03 09:21:52 +0100

    Initial commit

---
+++ Backend/src/main/java/utp/BroszuraProduktowa/BroszuraProduktowaApplication.java
@@ -0,0 +1,13 @@
+package utp.BroszuraProduktowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroszuraProduktowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroszuraProduktowaApplication.class, args);
+	}
+
+}
